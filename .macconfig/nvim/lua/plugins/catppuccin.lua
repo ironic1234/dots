a4b2c3fde7d00aef0a1483b6f0a495e948c24773
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2024-11-12 16:54:43 -0500

    feat(nvim): add italics

--- .macconfig/nvim/lua/plugins/catppuccin.lua
+++ .macconfig/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,21 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			transparent_background = true,
-		})
-	end,
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            transparent_background = true,
+            styles = {
+                comments = { "italic" },
+                conditionals = { "italic" },
+                loops = { "italic" },
+                functions = { "italic" },
+                properties = { "italic" },
+                types = { "italic" },
+            },
+            integrations = {
+                treesitter = true,
+            },
+        })
+    end,
 }
