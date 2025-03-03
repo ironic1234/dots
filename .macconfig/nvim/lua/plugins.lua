70da08f00a3c569ec2cf9d42e6fc9c833902eeee
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-03-03 14:26:35 -0500

    changed up configs

--- .macconfig/nvim/lua/plugins.lua
+++ .macconfig/nvim/lua/plugins.lua
@@ -1,37 +1,38 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    require("plugins.autopairs"),
-    require("plugins.autosave"),
-    require("plugins.catppuccin"),
-    require("plugins.telescope"),
-    require("plugins.treesitter"),
-    require("plugins.lsp"),
-    require("plugins.highlight-colors"),
-    require("plugins.alpha"),
-    require("plugins.vimtex"),
-    require("plugins.lualine"),
-    require("plugins.wit"),
-    require("plugins.oil"),
-    require("plugins.autotag"),
-    require("plugins.live-preview"),
-    require("plugins.neogit"),
-    require("plugins.undotree"),
-    require("plugins.zen-mode"),
-    require("plugins.rainbow-delimiters"),
+	require("plugins.autopairs"),
+	require("plugins.autosave"),
+	require("plugins.catppuccin"),
+	require("plugins.telescope"),
+	require("plugins.treesitter"),
+	require("plugins.lsp"),
+	require("plugins.highlight-colors"),
+	require("plugins.alpha"),
+	require("plugins.vimtex"),
+	require("plugins.lualine"),
+	require("plugins.wit"),
+	require("plugins.oil"),
+	require("plugins.autotag"),
+	require("plugins.live-preview"),
+	require("plugins.neogit"),
+	require("plugins.undotree"),
+	require("plugins.zen-mode"),
+	require("plugins.rainbow-delimiters"),
+	require("plugins.go-up"),
 }, {
-    ui = {
-        border = "rounded",
-    },
+	ui = {
+		border = "rounded",
+	},
 })
