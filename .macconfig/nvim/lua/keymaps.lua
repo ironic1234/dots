df589dbc9f5d9979afa732029c2629f8446841de
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-03-05 20:07:55 -0500

    feat(nvim): add cats :)

--- .macconfig/nvim/lua/keymaps.lua
+++ .macconfig/nvim/lua/keymaps.lua
@@ -23,28 +23,38 @@ vim.keymap.set("n", "<leader>gb", "<Cmd>e#<CR>")
 
 -- Autoquit insert mode
 vim.api.nvim_create_autocmd("CursorHoldI", {
-    pattern = "*",
-    callback = function()
-        vim.cmd("stopinsert")
-    end,
+	pattern = "*",
+	callback = function()
+		vim.cmd("stopinsert")
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertEnter", {
-    pattern = "*",
-    callback = function()
-        vim.b.updaterestore = vim.o.updatetime
-        vim.o.updatetime = 5000
-    end,
+	pattern = "*",
+	callback = function()
+		vim.b.updaterestore = vim.o.updatetime
+		vim.o.updatetime = 5000
+	end,
 })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
-    pattern = "*",
-    callback = function()
-        if vim.b.updaterestore then
-            vim.o.updatetime = vim.b.updaterestore
-        end
-    end,
+	pattern = "*",
+	callback = function()
+		if vim.b.updaterestore then
+			vim.o.updatetime = vim.b.updaterestore
+		end
+	end,
 })
 
 -- Undotree
 vim.keymap.set("n", "<leader>u", "<cmd>lua require('undotree').toggle()<cr>")
+
+vim.keymap.set("n", "<leader>cc", function()
+	require("duck").hatch("🐈")
+end, {})
+vim.keymap.set("n", "<leader>ck", function()
+	require("duck").cook()
+end, {})
+vim.keymap.set("n", "<leader>ca", function()
+	require("duck").cook_all()
+end, {})
