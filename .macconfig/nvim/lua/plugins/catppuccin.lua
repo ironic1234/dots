51e668e3da277a71096a5df426e564f45d709377
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-04-20 18:54:34 -0400

    updated theme

--- .macconfig/nvim/lua/plugins/catppuccin.lua
+++ .macconfig/nvim/lua/plugins/catppuccin.lua
@@ -15,12 +15,20 @@ return {
 			},
 			integrations = {
 				treesitter = true,
+				alpha = true,
+				markdown = true,
+				neotree = true,
+				cmp = true,
+				dap = true,
 			},
 			custom_highlights = function(colors)
 				return {
 					["@punctuation.bracket"] = { fg = colors.flamingo },
 					["@punctuation.delimiter"] = { fg = colors.flamingo },
 					["@punctuation.special"] = { fg = colors.flamingo },
+					LineNr = { fg = colors.peach },
+					WinSeparator = { fg = colors.peach },
+					VertSplit = { fg = colors.peach },
 				}
 			end,
 		})
