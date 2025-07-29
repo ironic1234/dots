a80a60e5b50b0d0dffd88a97978653c2583a4025
ironic1234 <55256196+ironic1234@users.noreply.github.com>
2025-07-29 18:23:21 -0400

    change autosave plugin

--- .macconfig/nvim/lua/plugins/autosave.lua
+++ .macconfig/nvim/lua/plugins/autosave.lua
@@ -1,14 +1,8 @@
 return {
-	"0x00-ketsu/autosave.nvim",
+	"okuuva/auto-save.nvim",
+	cmd = "ASToggle",
 	event = { "InsertLeave", "TextChanged" },
 	config = function()
-		require("autosave").setup({
-			enable = true,
-			prompt_style = "stdout",
-			prompt_message = function()
-				return "Autosave: saved at " .. vim.fn.strftime("%H:%M:%S")
-			end,
-			events = { "InsertLeave", "TextChanged" },
-		})
+		require("auto-save").setup()
 	end,
 }
