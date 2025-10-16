b2d0b737f44e62e0d2741ad7f781fcc6eb387e3b
Ronak Jain <ronak@ronakpjain.com>
2025-10-16 00:15:22 -0400

    clipboard and wayland for vscode

--- .linuxconfig/nvim/lua/basic.lua
+++ .linuxconfig/nvim/lua/basic.lua
@@ -21,7 +21,3 @@ vim.opt.clipboard:append("unnamedplus")
 vim.cmd.colorscheme("catppuccin")
 
 vim.g.have_nerd_font = true
-
-vim.g.python_host_prog = "/opt/homebrew/opt/python/libexec/bin/python"
-vim.g.python3_host_prog = "/opt/homebrew/opt/python/libexec/bin/python"
-vim.g.loaded_perl_provider = 0
