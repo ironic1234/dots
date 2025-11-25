a21be9d0b911e88cbd65b597432267652a09a689
Ronak Jain <ronak@ronakpjain.com>
2025-11-24 22:54:50 -0500

    feat(nvim): add Snacks image support

---
+++ .macconfig/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+
+	opts = {
+		image = {
+			enabled = true,
+			doc = {
+				conceal = function()
+					return false
+				end,
+			},
+		},
+	},
+}
