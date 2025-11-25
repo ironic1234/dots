6f9432b8612095d7caaaaa36e2008481406ae482
Ronak Jain <ronak@ronakpjain.com>
2025-11-25 17:51:07 -0500

    perf(nvim): reduce startup time

--- .macconfig/nvim/lua/plugins/markdown.lua
+++ .macconfig/nvim/lua/plugins/markdown.lua
@@ -1,11 +1,13 @@
 return {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
+		event = "BufReadPre",
 		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 		opts = {},
 	},
 	{
 		"Thiago4532/mdmath.nvim",
+		event = "BufReadPre",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 		},
--- .macconfig/nvim/lua/plugins/neogit.lua
+++ .macconfig/nvim/lua/plugins/neogit.lua
@@ -1,10 +1,11 @@
 return {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "sindrets/diffview.nvim",
-        "nvim-telescope/telescope.nvim",
-    },
-    opts = { graph_style = "kitty" },
-    config = true,
+	"NeogitOrg/neogit",
+	event = "VeryLazy",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+	opts = { graph_style = "kitty" },
+	config = true,
 }
