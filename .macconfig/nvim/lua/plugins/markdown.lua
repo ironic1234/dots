bc2d9573b98aff19628e9e883341bc74152c79f9
Ronak Jain <ronak@ronakpjain.com>
2025-11-25 14:47:50 -0500

    feat(nvim): add Markdown math and checkbox toggle

--- .macconfig/nvim/lua/plugins/markdown.lua
+++ .macconfig/nvim/lua/plugins/markdown.lua
@@ -1,5 +1,16 @@
 return {
-	"MeanderingProgrammer/render-markdown.nvim",
-	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+		opts = {},
+	},
+	{
+		"Thiago4532/mdmath.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
+	},
+	{
+		"opdavies/toggle-checkbox.nvim",
+	},
 }
