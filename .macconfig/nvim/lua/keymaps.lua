1363857b1463f14e88f4d18659529440e6239da4
Ronak Jain <ronak@ronakpjain.com>
2026-01-01 18:36:34 -0500

    add keymap stuff (descriptions and whichkey)

--- .macconfig/nvim/lua/keymaps.lua
+++ .macconfig/nvim/lua/keymaps.lua
@@ -4,103 +4,91 @@ vim.g.maplocalleader = " "
 
 -- Telescope keymaps
 local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
 -- Oil keymap
-vim.keymap.set("n", "<leader>fo", vim.cmd.Oil, {})
+vim.keymap.set("n", "<leader>fo", vim.cmd.Oil, { desc = "Open Oil file manager" })
 
 -- Alpha keymap
-vim.keymap.set("n", "<leader>fa", vim.cmd.Alpha)
+vim.keymap.set("n", "<leader>fa", vim.cmd.Alpha, { desc = "Open Alpha dashboard" })
 
 -- StartupTime keymap
-vim.keymap.set("n", "<leader>fs", "<Cmd>Lazy profile<CR>")
+vim.keymap.set("n", "<leader>fs", "<Cmd>Lazy profile<CR>", { desc = "Show Lazy profile/Startup time" })
 
 -- Go back to previous file
-vim.keymap.set("n", "<leader>gb", "<Cmd>e#<CR>")
+vim.keymap.set("n", "<leader>gb", "<Cmd>e#<CR>", { desc = "Go to previous buffer" })
 
 -- Undotree
 vim.keymap.set("n", "<leader>u", function()
 	require("undotree").toggle()
-end)
+end, { desc = "Toggle Undotree" })
 
+-- Duck
 vim.keymap.set("n", "<leader>cc", function()
 	require("duck").hatch("🐈")
-end, {})
+end, { desc = "Hatch a duck (cat)" })
 vim.keymap.set("n", "<leader>ck", function()
 	require("duck").cook()
-end, {})
+end, { desc = "Cook a duck" })
 vim.keymap.set("n", "<leader>ca", function()
 	require("duck").cook_all()
-end, {})
+end, { desc = "Cook all ducks" })
 
 local dap = require("dap")
 
--- Basic controls
-vim.keymap.set("n", "<leader>bp", dap.toggle_breakpoint, {}) -- breakpoint
-vim.keymap.set("n", "<leader>bc", dap.continue, {}) -- continue
-vim.keymap.set("n", "<leader>br", dap.restart, {}) -- restart
-vim.keymap.set("n", "<leader>bt", dap.terminate, {}) -- terminate
+-- DAP: Basic controls
+vim.keymap.set("n", "<leader>bp", dap.toggle_breakpoint, { desc = "Debug: Toggle breakpoint" })
+vim.keymap.set("n", "<leader>bc", dap.continue, { desc = "Debug: Continue" })
+vim.keymap.set("n", "<leader>br", dap.restart, { desc = "Debug: Restart" })
+vim.keymap.set("n", "<leader>bt", dap.terminate, { desc = "Debug: Terminate" })
 vim.keymap.set("n", "<leader>bb", function()
 	dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-end, {}) -- conditional breakpoint
+end, { desc = "Debug: Set conditional breakpoint" })
 
--- Stepping
-vim.keymap.set("n", "<leader>bs", dap.step_over, {}) -- step over
-vim.keymap.set("n", "<leader>bi", dap.step_into, {}) -- step into
-vim.keymap.set("n", "<leader>bo", dap.step_out, {}) -- step out
-vim.keymap.set("n", "<leader>bx", dap.run_to_cursor, {}) -- run to cursor
+-- DAP: Stepping
+vim.keymap.set("n", "<leader>bs", dap.step_over, { desc = "Debug: Step over" })
+vim.keymap.set("n", "<leader>bi", dap.step_into, { desc = "Debug: Step into" })
+vim.keymap.set("n", "<leader>bo", dap.step_out, { desc = "Debug: Step out" })
+vim.keymap.set("n", "<leader>bx", dap.run_to_cursor, { desc = "Debug: Run to cursor" })
 
--- REPL
-vim.keymap.set("n", "<leader>brl", dap.repl.open, {}) -- open REPL
-vim.keymap.set("n", "<leader>brq", dap.repl.close, {}) -- close REPL
+-- DAP: REPL
+vim.keymap.set("n", "<leader>brl", dap.repl.open, { desc = "Debug: Open REPL" })
+vim.keymap.set("n", "<leader>brq", dap.repl.close, { desc = "Debug: Close REPL" })
 
--- UI toggle (if using dap-ui)
+-- DAP: UI toggle
 local dapui_ok, dapui = pcall(require, "dapui")
 if dapui_ok then
-	vim.keymap.set("n", "<leader>bu", dapui.toggle, {}) -- UI toggle
+	vim.keymap.set("n", "<leader>bu", dapui.toggle, { desc = "Debug: Toggle UI" })
 end
 
 -- Dropbar
 local dropbar_api = require("dropbar.api")
-vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Dropbar: Pick symbols in winbar" })
+vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Dropbar: Go to start of current context" })
+vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Dropbar: Select next context" })
 
 -- LSP
 vim.keymap.set("n", "gd", function()
 	vim.lsp.buf.definition()
-end)
+end, { desc = "LSP: Go to definition" })
 vim.keymap.set("n", "gi", function()
 	vim.lsp.buf.implementation()
-end)
+end, { desc = "LSP: Go to implementation" })
 vim.keymap.set("n", "gh", function()
 	vim.lsp.buf.hover()
-end)
+end, { desc = "LSP: Hover documentation" })
 vim.keymap.set("n", "gD", function()
 	vim.diagnostic.open_float()
-end)
+end, { desc = "LSP: Open diagnostic float" })
 vim.keymap.set("n", "gr", function()
 	vim.lsp.buf.references()
-end)
+end, { desc = "LSP: List references" })
 vim.keymap.set("n", "ga", function()
 	vim.lsp.buf.code_action()
-end)
+end, { desc = "LSP: Code actions" })
 vim.keymap.set("n", "<leader>l", function()
 	vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
-end)
-
--- MdMath
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "tex",
-	callback = function()
-		require("mdmath").enable()
-	end,
-})
-
--- Toggle Checkbox
-vim.keymap.set({ "n", "i" }, "<A-'>", function()
-	require("toggle-checkbox").toggle()
-end)
+end, { desc = "LSP: Toggle inlay hints" })
