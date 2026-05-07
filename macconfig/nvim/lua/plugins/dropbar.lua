3cd0f48e304fe6f4c55edd3e7a4a97b5db5f1f52
Ronak Jain <ronak@ronakpjain.com>
2026-05-07 14:31:56 -0400

    chore(pi): update borders, Dropbar, and Pi settings

--- macconfig/nvim/lua/plugins/dropbar.lua
+++ macconfig/nvim/lua/plugins/dropbar.lua
@@ -1,9 +1,13 @@
 vim.pack.add({
 	"https://github.com/nvim-telescope/telescope-fzf-native.nvim",
-	"https://github.com/Bekaboo/dropbar.nvim",
+	-- "https://github.com/Bekaboo/dropbar.nvim",
 }, { confirm = false, load = true })
 
 local fzf_native_path = vim.fn.stdpath("data") .. "/site/pack/core/opt/telescope-fzf-native.nvim"
-if vim.fn.executable("make") == 1 and vim.uv.fs_stat(fzf_native_path) and not vim.uv.fs_stat(fzf_native_path .. "/build") then
+if
+	vim.fn.executable("make") == 1
+	and vim.uv.fs_stat(fzf_native_path)
+	and not vim.uv.fs_stat(fzf_native_path .. "/build")
+then
 	vim.system({ "make" }, { cwd = fzf_native_path }):wait()
 end
